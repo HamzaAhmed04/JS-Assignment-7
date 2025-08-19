fd18058e07acf6cca6a06bc3fa28edfdddd74a99
Hamza Ahmed Shaikh <99156466+HamzaAhmed04@users.noreply.github.com>
2025-08-19 13:47:35 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,72 @@
+// // Q1
+
+// var arr = [[], [], []];  
+
+
+
+// // Q2
+
+// var arr = [
+//   [0, 1, 2, 3],
+//   [1, 0, 1, 2],
+//   [2, 1, 0, 1]
+// ];
+
+// console.log (arr)
+
+
+// // Q3
+
+// for (var i = 1; i <=10; i++) {
+//         console.log (i)
+//     }
+
+
+// // Q4
+
+// var choseNum = prompt ("Enter a number to show its multiplication table")
+// var tableLength = prompt ("Enter length multiplication table")
+
+// for ( var i = choseNum; i <=tableLength; i++){
+    // console.log (choseNum + " x " + i + " = " + (choseNum * i))
+// }
+
+
+// // Q5
+
+// var fruits = ["apple", "banana", "mango", "orange",
+// "strawberry"]
+//     console.log (fruits [0] , fruits [1], fruits [2] , fruits [3] , fruits [4])
+
+
+// // Q6
+
+// // for ( var i = 1; i <=15; i++){
+// //     console.log (i)
+// // }
+
+// for ( var i = 10; i>=1; i--){
+//     console.log (i)
+// }
+
+// // Q7
+
+// var A = ["cake", "apple pie", "cookie", "chips", "patties"];
+// var userInput = prompt ("What do you want").toLowerCase()
+
+// found = false
+
+// for (var i = 0; i < A.length; i++) {
+//     if(A[i] === userInput) {
+//         found = true
+//         break
+//     }
+// }
+
+// if (found) {
+//     alert("Shapar double kardon?")
+// }
+
+// else {
+//     alert ("ye piche se hi nahi araha hai")
+// }
